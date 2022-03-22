f2823b4435cf0d5ad1e1ce35386b9777dd876738
Glitch (prajwol-playlist-project) <none>
2022-03-22 21:27:54 +0000

    ⚕️🧳 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,29 +1,9 @@
 // Complete Day 1 goals here
 
-//Old Day 1 code
-/*
-var songs = ["Over", "Le Parjure", "Lookin Up", "Cruel Dilemme", "Letting Go"];
-var images = [
-  "https://i1.sndcdn.com/artworks-000066084977-b06t2e-t500x500.jpg",
-  "https://i1.sndcdn.com/artworks-000145746186-ylr3tx-t500x500.jpg",
-  "https://images.genius.com/7f2fe4ef3fec3fce5485ca09872259d7.500x500x1.jpg",
-  "https://i.scdn.co/image/ab67616d0000b27343fd757ae08d4dd4ce338b56",
-  "https://images.genius.com/7f2fe4ef3fec3fce5485ca09872259d7.500x500x1.jpg",
-];
-var artists = ["Our Samplus", "Our Samplus", "Potsu", "Shiro Sagisu"];
-var lengths = ["4:05mins", "3:26mins", "2:39mins", "2:46mins", "3:18mins"];
-var links = [
-  "https://youtu.be/Kbf9TzlRW0A",
-  "https://youtu.be/a9qTQ0n1Hxk",
-  "https://youtu.be/2Bo7cQb9FLs",
-  "https://youtu.be/kvyVhbqFqDE",
-  "https://youtu.be/o7zEwssiSVE",
-];*/
-
-//New Day 1 code
+//Day 1 code
 let mySongs = [
   {
-    song: "Over",
+    song: "Over", 
     artist: "Our Samplus",
     runtime: "4:05mins",
     link: "https://youtu.be/Kbf9TzlRW0A",
@@ -63,71 +43,65 @@ let mySongs = [
 ];
 
 function displaySongInfo() {
-  
   mySongs.forEach(function (song) {
     $(".songs").append(`<p> ${song.song} </p>`);
   });
-  
-    mySongs.forEach(function (artist) {
+
+  mySongs.forEach(function (artist) {
     $(".artists").append(`<p> ${artist.artist} </p>`);
   });
-  
-    mySongs.forEach(function (runtime) {
+
+  mySongs.forEach(function (runtime) {
     $(".lengths").append(`<p>${runtime.runtime}</p>`);
   });
-  
-    mySongs.forEach(function (link) {
+
+  mySongs.forEach(function (link) {
     $(".links").append(`<a href=" ${link.link}" target="_blank">Test</a>`);
   });
-  
-    mySongs.forEach(function (coverArt) {
+
+  mySongs.forEach(function (coverArt) {
     $(".images").append(`<img src=${coverArt.coverArt}>`);
   });
-  }
-
-/*function displaySongInfo() {
-  // Complete the Day 2 goals inside this function
-  songs.forEach(function (song) {
-    $(".songs").append(`<li> ${song} </li>`);
-  });
-
-  images.forEach(function (image) {
-    $(".images").append(`<img src=${image}>`);
-  });
-
-  artists.forEach(function (artist) {
-    $(".artists").append(`<li> ${artist} </li>`);
-  });
-
-  lengths.forEach(function (length) {
-    $(".lengths").append(`<li> ${length} </li>`);
-  });
-
-  links.forEach(function (link) {
-    $(".links").append(`<a href=" ${link}" target="_blank">Test</a>`);
-  });
-}*/
-
-function emptySongInfo() {
-  /* this function empties the divs each time the button is 
-  clicked so that your playlist does not repeatedly add the data
-  too many times. Try commenting out this function call to see 
-  what happens without it! */
-  $(".song").empty();
-  $(".coverArt").empty();
-  $(".artist").empty();
-  $(".runtime").empty();
-  $(".link").empty();
 }
 
 function addSongInfo() {
   // Complete Day 3 goals inside this function
 }
 
-$("#add").click(function () {
+displaySongInfo();
+
+//Day 3 Code
+$(".add").click(function () {
   emptySongInfo();
-  addSongInfo();
+  var song = $(".title").val();
+  var artist = $(".artist").val();
+  var runtime = $(".length").val();
+  var link = $(".link").val();
+  var coverArt = $(".image").val();
+   
+  mySongs.push({
+    song: song, 
+    artist: artist,
+    runtime: runtime,
+    link: link,
+    coverArt: coverArt,
+    
+  },);
+      console.log(song)
+  
   displaySongInfo();
+  
 });
 
-displaySongInfo();
+function emptySongInfo() {
+  /* this function empties the divs each time the button is 
+  clicked so that your playlist does not repeatedly add the data
+  too many times. Try commenting out this function call to see 
+  what happens without it! */
+  $(".songs").empty();
+  $(".images").empty();
+  $(".artists").empty();
+  $(".lengths").empty();
+  $(".links").empty();
+}
+
